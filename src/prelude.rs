cac2872351c86c20dd1d79b29087e6fc2c7a02c9
Eagle <eagle@pop-os.localdomain>
2023-10-18 09:54:50 +0100

    Added some docs

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,12 +5,15 @@ use crate::sequence::types::TransitionFunction;
 pub use crate::sequence::{states::*, Sequence, SharedSequenceBehavior};
 pub use crate::sequence_part::{states::*, SequencePart};
 
+
+/// Create new sequence that do not require initial elements
 pub fn seq_without_initial_elements<T>(
     transition_function: TransitionFunction<T, WithoutInitialElements>,
 ) -> Sequence<T, WithoutInitialElements, WithTransitionFunction<T, WithoutInitialElements>> {
     Sequence::new().transition_function(transition_function)
 }
 
+/// Create new sequence that do require initial elements
 pub fn seq_with_initial_elements<T>(
     initial_elements: Vec<T>,
     transition_function: TransitionFunction<T, WithInitialElements>,
