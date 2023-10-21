340a2220c8b240bc473321e9c039f2a1de02855b
Eagle <eagle@pop-os.localdomain>
2023-10-21 13:00:34 +0100

    Mod

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@
 
 #![deny(missing_docs)]
 #![doc(
-    html_logo_url = "../seqgen_logo.svg",
-    html_favicon_url = "../seqgen_logo.svg"
+    html_logo_url = "/seqgen_logo.svg",
+    html_favicon_url = "/seqgen_logo.svg"
 )]
 
 pub mod error;
