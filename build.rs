00e4b7570b4b48b915476bc378229f65b23e9322
Eagle <eagle@pop-os.localdomain>
2023-10-21 12:36:21 +0100

    Mod

--- build.rs
+++ build.rs
@@ -2,12 +2,6 @@ fn main() {
     // Copy the images to the output when generating documentation
     println!("cargo:rerun-if-changed=assets/doc");
 
-    let res = std::env::current_dir();
-    match res {
-        Ok(path) => println!("{}", path.into_os_string().into_string().unwrap()),
-        Err(_) => (),
-    };
-
     std::fs::copy("../../../assets/seqgen_logo.svg", "target/doc/seqgen_logo.svg")
         .expect("Failed to copy crate logo when building documentation.");
 }
