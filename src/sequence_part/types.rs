e9e55f79b103a57f331180a7c0c763e455e455c7
Eagle <eagle@pop-os.localdomain>
2023-10-28 12:23:02 +0100

    Added types for squence_part

---
+++ src/sequence_part/types.rs
@@ -0,0 +1,23 @@
+//! This module defines type aliases for sequence part
+
+use super::states::{AliveElements, Range};
+use super::SequencePart;
+use crate::error::RangeError;
+use crate::sequence::{states::WithTransitionFunction, Sequence};
+
+/// An immutable reference to the parent sequence.
+pub type ParentSequenceRef<'a, T, I> = &'a Sequence<T, I, WithTransitionFunction<T, I>>;
+
+/// A mutable reference to the parent sequence.
+pub type ParentSequenceRefMut<'a, T, I> = &'a mut Sequence<T, I, WithTransitionFunction<T, I>>;
+
+/// Alive elements part.
+pub type AliveElementsPart<'a, T, I> =
+    SequencePart<AliveElements, &'a Sequence<T, I, WithTransitionFunction<T, I>>>;
+
+/// Range part type.
+pub type RangePart<'a, T, I> =
+    SequencePart<Range, &'a mut Sequence<T, I, WithTransitionFunction<T, I>>>;
+
+/// Range result that is returned when creating ranges.
+pub type RangeResult<'a, T, I> = Result<RangePart<'a, T, I>, RangeError>;
